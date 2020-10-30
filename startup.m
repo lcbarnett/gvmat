d1a24d318ed87518c35e6b355908da44c8233199
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2020-10-30 17:30:15 +0000

    Amended startup.m

--- startup.m
+++ startup.m
@@ -1,1 +1,10 @@
-% TODO - do nothing for now
+% Startup stuff
+
+fprintf('[gvmat startup] Initialising gvmat\n');
+
+global gvmat_root;
+gvmat_root = fileparts(mfilename('fullpath')); % directory containing this file
+addpath(gvmat_root);
+fprintf('[gvmat startup] Added path %s\n',gvmat_root);
+
+fprintf('[gvmat startup] Initialised (you may re-run `startup'' at any time)\n');
