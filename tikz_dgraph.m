5f2d236137b197b072a7883acfba7ffe6e93cb4c
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2025-06-24 16:34:31 +0100

    Amended tikz routines to use generated color scales

--- tikz_dgraph.m
+++ tikz_dgraph.m
@@ -1,4 +1,4 @@
-function tikz_dgraph(texfile,A,pos,csize,psize,ebend,cm)
+function tikz_dgraph(texfile,A,pos,csize,psize,ebend,csp)
 
 % A(i,j) is j -> i
 
@@ -6,8 +6,6 @@ function tikz_dgraph(texfile,A,pos,csize,psize,ebend,cm)
 assert(ismatrix(A) && n1 == n,'Adjacency matrix must be a square matrix');
 [n2,n3] = size(pos);
 assert(ismatrix(pos) && n2 == n && n3 == 2,'Positions must be an n x 2 matrix where n is the number of nodes');
-
-ncols = size(cm,1);
 
 [fd,emsg] = fopen(texfile,'w+t');
 assert(fd ~= -1,'failed to open tikz LaTeX file ''%'': %s',texfile,emsg);
@@ -26,8 +24,8 @@ fprintf(fd,'\\end{scope}\n');
 fprintf(fd,'\\begin{scope}[>={Stealth},every node/.style={fill=white,circle},every edge/.style={draw,very thick}]\n');
 for i = 2:n
 	for j = 1:i-1
-		cij = floor(255*cm(mapcol(A(i,j),ncols),:));
-		cji = floor(255*cm(mapcol(A(j,i),ncols),:));
+		cij = cs_cubic_red(A(i,j),0,1,csp);
+		cji = cs_cubic_red(A(j,i),0,1,csp);
 		if A(i,j) ~= 0
 			if A(j,i) ~= 0
 				fprintf(fd,'\t\\path[color={rgb,255:red,%3d;green,%3d;blue,%3d},->] (%d) edge[bend right =  %d] (%d);\n',cij(1),cij(2),cij(3),j,ebend,i);
@@ -56,4 +54,30 @@ function i = mapcol(x,N)
 	if i > N
 		i = N;
 	end
+end
+
+function rgb = cs_linear_red(x,xlo,xhi,~)
+
+	y = (x-xlo)./(xhi-xlo);
+	rgb = ones(length(x),3);
+	rgb(:,1) = 1;
+	rgb(:,2) = 1-y;
+	rgb(:,3) = 1-y;
+	rgb = round(255*rgb);
+
+end
+
+function rgb = cs_cubic_red(x,xlo,xhi,p)
+
+	a = 1+2*(1-p);
+	b = 3*(1-a);
+	c = 1-a-b;
+	y = (x-xlo)./(xhi-xlo);
+	y = a*y + b*y.^2 + c*y.^3;
+	rgb = ones(length(x),3);
+	rgb(:,1) = 1;
+	rgb(:,2) = 1-y;
+	rgb(:,3) = 1-y;
+	rgb = round(255*rgb);
+
 end
--- tikz_ugraph.m
+++ tikz_ugraph.m
@@ -1,11 +1,9 @@
-function tikz_ugraph(texfile,A,pos,csize,psize,cm)
+function tikz_ugraph(texfile,A,pos,csize,psize,csp)
 
 [n,n1] = size(A);
 assert(ismatrix(A) && n1 == n,'Adjacency matrix must be a square matrix');
 [n2,n3] = size(pos);
 assert(ismatrix(pos) && n2 == n && n3 == 2,'Positions must be an n x 2 matrix where n is the number of nodes');
-
-ncols = size(cm,1);
 
 [fd,emsg] = fopen(texfile,'w+t');
 assert(fd ~= -1,'failed to open tikz LaTeX file ''%'': %s',texfile,emsg);
@@ -24,7 +22,7 @@ fprintf(fd,'\\end{scope}\n');
 fprintf(fd,'\\begin{scope}[>={Stealth},every node/.style={fill=white,circle},every edge/.style={draw,very thick}]\n');
 for i = 2:n
 	for j = 1:i-1
-		cij = floor(255*cm(mapcol(A(i,j),ncols),:));
+		cij = cs_cubic_bluered(A(i,j),0,1,csp);
 		if A(i,j) ~= 0
 			fprintf(fd,'\t\\path[color={rgb,255:red,%3d;green,%3d;blue,%3d},-] (%d) edge (%d);\n',cij(1),cij(2),cij(3),i,j);
 		end
@@ -44,4 +42,45 @@ function i = mapcol(x,N)
 	if i > N
 		i = N;
 	end
+end
+
+function rgb = cs_linear_bluered(x,xlo,xhi,~)
+
+	y = 2*(x-xlo)./(xhi-xlo)-1;
+	rgb = ones(length(x),3);
+	if y >= 0
+		rgb(:,1) = 1;
+		rgb(:,2) = 1-y;
+		rgb(:,3) = 1-y;
+	else
+		rgb(:,1) = 1+y;
+		rgb(:,2) = 1+y;
+		rgb(:,3) = 1;
+	end
+	rgb = round(255*rgb);
+
+end
+
+function rgb = cs_cubic_bluered(x,xlo,xhi,p)
+
+	a = 1+2*(1-p);
+	b = 3*(1-a);
+	c = 1-a-b;
+	y = (x-xlo)./(xhi-xlo);
+	y = 2*y-1;
+	rgb = ones(length(x),3);
+	if y >= 0
+		y = a*y + b*y.^2 + c*y.^3;
+		rgb(:,1) = 1;
+		rgb(:,2) = 1-y;
+		rgb(:,3) = 1-y;
+	else
+		y = -y;
+		y = a*y + b*y.^2 + c*y.^3;
+		rgb(:,1) = 1-y;
+		rgb(:,2) = 1-y;
+		rgb(:,3) = 1;
+	end
+	rgb = round(255*rgb);
+
 end
